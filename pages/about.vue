131788681067d4e13586905810b3fdfea73dab09
parktaegune <qkrxo876@naver.com>
2018-05-08 12:55:19 +0900

    (2018-05-08) init testjs

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <div class = "block">
+      <a href = "#" class = "button is-primary">Test</a>
+      <p>클라우드 서비스용 포트폴리오</p>
+      <img src="~static/logo.gif" alt=""/><br>
+      
+    </div>
+    <br>
+    <nuxt >
+  </section>
+</template>
+
+<script>
+import Vue from 'vue';
+</script>
