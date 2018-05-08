98b0d7d392502c0981312ca1fb26d59186bb36e6
parktaegune <qkrxo876@naver.com>
2018-05-08 12:58:46 +0900

    (2018-05-08-4) init testjs

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <a href = "#" class = "button is-primary">Test</a>
       <p>클라우드 서비스용 포트폴리오</p>
       <img src="~static/logo.gif" alt=""/><br>
-      <img src="~assets/cutomLogo.gif" alt=""><br>
+      
     </div>
     <br>
     <nuxt >
